b3151ed43721e95c9d71fa198e2cb6aea363233c
Taiki Kamishima <st042306@m02.kyoto-kcg.ac.jp>
2024-11-21 15:59:51 +0900

    First commit

---
+++ J1.js
@@ -0,0 +1,128 @@
+function J1_show() {
+    $(".J1").show();
+    $(".J1_info div").hide();
+    $("h2").css("background-color", "white").css("color", "black");
+}
+
+function sapporo() {
+    $(".J1").hide();
+    $(".J1_info #sapporo").show();
+    $("h2").css("background-color", "#CF0000").css("color", "black");
+}
+
+function kashima() {
+    $(".J1").hide();
+    $(".J1_info #kashima").show();
+    $("h2").css("background-color", "#B71840").css("color", "white");
+}
+
+function urawa() {
+    $(".J1").hide();
+    $(".J1_info #urawa").show();
+    $("h2").css("background-color", "#E6002D").css("color", "black");
+}
+
+function kashiwa() {
+    $(".J1").hide();
+    $(".J1_info #kashiwa").show();
+    $("h2").css("background-color", "#FFF100").css("color", "black");
+}
+
+function fctokyo() {
+    $(".J1").hide();
+    $(".J1_info #fctokyo").show();
+    $("h2").css("background-color", "#000A6E").css("color", "#FC0000");
+}
+
+function verdy() {
+    $(".J1").hide();
+    $(".J1_info #verdy").show();
+    $("h2").css("background-color", "#046A38").css("color", "white");
+}
+
+function machida() {
+    $(".J1").hide();
+    $(".J1_info #machida").show();
+    $("h2").css("background-color", "#05488B").css("color", "white");
+}
+
+function kawasaki() {
+    $(".J1").hide();
+    $(".J1_info #kawasaki").show();
+    $("h2").css("background-color", "#23B9FE").css("color", "white");
+}
+
+function marinos() {
+    $(".J1").hide();
+    $(".J1_info #marinos").show();
+    $("h2").css("background-color", "#004098").css("color", "white");
+}
+
+function shonan() {
+    $(".J1").hide();
+    $(".J1_info #shonan").show();
+    $("h2").css("background-color", "#60B424").css("color", "#005CA2");
+}
+
+function niigata() {
+    $(".J1").hide();
+    $(".J1_info #niigata").show();
+    $("h2").css("background-color", "#FF6325").css("color", "#014099");
+}
+
+function iwata() {
+    $(".J1").hide();
+    $(".J1_info #iwata").show();
+    $("h2").css("background-color", "#7399D1").css("color", "white");
+}
+
+function nagoya() {
+    $(".J1").hide();
+    $(".J1_info #nagoya").show();
+    $("h2").css("background-color", "#D70C18").css("color", "#F8B500");
+}
+
+function kyoto() {
+    $(".J1").hide();
+    $(".J1_info #kyoto").show();
+    /* もしくは
+        $("h2").css("background-color", "#6F0467").css("color", "#E5002D");
+    */
+    $("h2").css("background-color", "#6F0467").css("color", "white");
+}
+
+function gamba() {
+    $(".J1").hide();
+    $(".J1_info #gamba").show();
+    $("h2").css("background-color", "#003287").css("color", "white");
+}
+
+function cerezo() {
+    $(".J1").hide();
+    $(".J1_info #cerezo").show();
+    $("h2").css("background-color", "#DA005C").css("color", "#1E2380");
+}
+
+function kobe() {
+    $(".J1").hide();
+    $(".J1_info #kobe").show();
+    $("h2").css("background-color", "#980323").css("color", "white");
+}
+
+function hiroshima() {
+    $(".J1").hide();
+    $(".J1_info #hiroshima").show();
+    $("h2").css("background-color", "#51318F").css("color", "white");
+}
+
+function fukuoka() {
+    $(".J1").hide();
+    $(".J1_info #fukuoka").show();
+    $("h2").css("background-color", "#00205B").css("color", "white");
+}
+
+function tosu() {
+    $(".J1").hide();
+    $(".J1_info #tosu").show();
+    $("h2").css("background-color", "#2196F3").css("color", "#E95599");
+}
